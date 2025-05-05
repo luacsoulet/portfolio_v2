5e30efb400a57b20c26ee84ec58c2dcf6964a62c
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-05 19:04:57 +0200

    Create an HeroSection components
    Create a HeroSection component based on DaisyUI’s prebuilt hero section, and import the RotatingText component.

---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import RotatingText from "./RotatingText";
+
+export const HeroSection = () => {
+    return (
+        <div className="hero-content min-w-full p-24 justify-start">
+            <div className="max-w-fit text-left">
+                <h1 className="text-5xl">Hi, I'm Lucas</h1>
+                <RotatingText
+                    texts={["React Developer", "Node.js Developer", "Full Stack Developer"]}
+                    rotationInterval={4000}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    staggerDuration={0.02}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                    splitLevelClassName="text-5xl font-bold text-[#E4E6C3]"
+                />
+            </div>
+        </div>
+    );
+};
