22a6850d6f79fbb4b45fe878d3d5618724276fc1
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-10 01:48:16 +0200

    Minor changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { HeroSection } from "@/components/HeroSection";
-
+import { PortfolioSection } from "@/components/PortfolioSection";
+import { AboutSection } from "@/components/AboutSection";
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen hero">
+    <div className="flex flex-col items-center h-full gap-24">
       <HeroSection />
+      <PortfolioSection />
+      <AboutSection />
     </div>
   );
 }
